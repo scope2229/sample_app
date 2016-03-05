479586608183ce7b14843e5216438b523e4b623d
kaine <kaine_76@hotmail.com>
2016-03-06 03:43:59 +0800

    commiting the help from irc

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
   
 
+  get 'static_pages/help' => 'static_pages#help'
+
+  get 'static_pages/about' => 'static_pages#about'
+
+  get 'static_pages/contact' => 'static_pages#contact'
+  
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,28 +1,32 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup 
+    @base_title = "Micro-Blog"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Micro-Blog | Home"
+    assert_select "title", "#{@base_title} | Home"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Micro-Blog | Help"
+    assert_select "title", "#{@base_title} | Help"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "Micro-Blog | About"
+    assert_select "title", "#{@base_title} | About"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Micro-Blog | Contact"
+    assert_select "title", "#{@base_title} | Contact"
   end
 
 end
