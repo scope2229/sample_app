fc92036d5a303af43d4a361b1f7fa79203352504
kaine <kaine_76@hotmail.com>
2016-03-13 18:19:10 +0800

    add full ittle helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = 'Home')
+		base_title = "Micro-Blog"
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -8,25 +8,25 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "#{@base_title} | Home"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "#{@base_title} | Help"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "#{@base_title} | About"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "#{@base_title} | Contact"
+    assert_select "title", "#{@base_title}"
   end
 
 end
